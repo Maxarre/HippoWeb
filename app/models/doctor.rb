9bccb10f17f53e48d231595e88f8b5d16b899db0
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-06-03 15:17:45 +0200

    models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,7 @@ class Doctor < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :patients
+  has_many :consultations, through: :patients
 end
