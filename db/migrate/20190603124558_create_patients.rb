781c228cb8544b561cd3565bd274382256d5ce06
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-06-03 15:02:22 +0200

    models

---
+++ db/migrate/20190603124558_create_patients.rb
@@ -0,0 +1,17 @@
+class CreatePatients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :patients do |t|
+      t.references :doctor, foreign_key: true
+      t.string :gender
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :job_title
+      t.string :address
+      t.date :date_of_birth
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_121559) do
+ActiveRecord::Schema.define(version: 2019_06_03_125451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "consultations", force: :cascade do |t|
+    t.bigint "patient_id"
+    t.string "status"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "type"
+    t.text "required_documents"
+    t.text "patient_complain"
+    t.text "diagnostic"
+    t.text "care_plan"
+    t.text "prescription"
+    t.text "note_patient"
+    t.string "patient_note_attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_consultations_on_patient_id"
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "doctor_phone"
+    t.string "photo"
+    t.string "office_address"
+    t.text "office_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_doctors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.bigint "doctor_id"
+    t.string "gender"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.string "job_title"
+    t.string "address"
+    t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_patients_on_doctor_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +78,6 @@ ActiveRecord::Schema.define(version: 2019_06_03_121559) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "consultations", "patients"
+  add_foreign_key "patients", "doctors"
 end
