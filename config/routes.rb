f81dc9712823b5152a51c954301f87cb8b7d6617
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-06-03 17:12:09 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,17 @@ Rails.application.routes.draw do
   devise_for :doctors
   root to: 'pages#home'
   get "dashboard", to: "doctors#dashboard"
+  get "patients/:id/consultations/new", to: "consultations#new"
+  post "patients/:id/consultations", to: "consultations#create"
+  get "consultations/:id/emails/new", to: "emails#new"
+  post "consultations/:id/emails", to: "email#create"
+  get "/dashboard", to: "doctors#dashboard"
+  get "patients/:id", to: "patients#show"
+  get "consultations/:id/emails", to: "emails#index"
+  get "emails/:id", to: "emails/:id#show"
+  get "/patients/:id/edit", to: "patients#edit"
+  patch "/patients/:id", to: "patients#update"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
