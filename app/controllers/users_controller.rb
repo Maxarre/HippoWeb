9ca62257f809bec8b1f1dca370e32b4162f6013f
Martin SION <martin@sionfamily.com>
2019-06-04 15:53:28 +0200

    fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit
+    params.require(:user).permit(:first_name, :last_name, :phone, :photo, :office_adress, :office_info)
   end
 end
