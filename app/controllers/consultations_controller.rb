673ab645ba77a381c90a4372a37cb90b27b3fa31
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-06-10 17:07:04 +0200

    this is the real 'follow-up edit'

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -20,15 +20,16 @@ class ConsultationsController < ApplicationController
 
   def edit
     # @consultation = Consultation.find(params[:id])
-    @consultation = Consultation.last
+    # @consultation = Consultation.last
     # @consultation = Consultation.order('id desc').offset(1).first --> for the last - 1
     @patient = Patient.find(params[:id])
+    @consultation = @patient.consultations.last
     # @consultation = @patient.consultations.where("consultation.consultation-type", "pending").first
   end
 
   def update
-    # @consultation = Consultation.find(params[:id])
-    @consultation = Consultation.last
+    @consultation = Consultation.find(params[:id])
+    # @consultation = Consultation.last
     # @consultation = Consultation.order('id desc').offset(1).first --> for the last - 1
     @consultation.update(consultation_params)
     @consultation.patient = Patient.find(params[:patient_id])
