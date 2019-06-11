34a04163ffbc5534cd72e4f25c22f49ee7079e6d
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-06-11 12:03:44 +0200

    Ajax Biatch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,12 @@ import "../plugins/flatpickr";
 import 'select2/dist/css/select2.css';
 
 import { initSelect2 } from '../plugins/init_select2';
+import { initAjax } from '../components/init_ajax';
 
 initSelect2();
 
 import $ from 'jquery';
 
 window.$ = $
+
+global.initAjax = initAjax;
