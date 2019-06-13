1e6ef31fd0e239db8c9afd44b81f1abe8555c000
Tom Halimi <tomcompteperso@gmail.com>
2019-06-13 16:49:23 +0200

    branch tom

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,71 +1,71 @@
-class ConsultationsController < ApplicationController
-  def index
-    @consultations = Consultation.all
-    @consultation = Consultation.new(params[:consultation])
-  end
-
-  def show
-    @consultation = Consultation.find(params[:id])
-  end
-
-  def new
-    @consultation = Consultation.new
-  end
-
-  def create
-    @consultation = Consultation.new(consultation_params)
-    @consultation.save
-    redirect_to dashboard_path
-  end
-
-  def edit
-    @patient = Patient.find(params[:patient_id])
-    @consultation = @patient.consultations.last
-    @consultation_templates = YAML.load_file("db/consultation_templates.yml")
-    @consultation.patient = Patient.find(params[:patient_id])
-    @all_tagg_list = ActsAsTaggableOn::Tag.all
-    respond_to do |format|
-      format.html { redirect_to edit_patient_consultation_path(@patient) }
-      format.js
+  class ConsultationsController < ApplicationController
+    def index
+      @consultations = Consultation.all
+      @consultation = Consultation.new(params[:consultation])
     end
-  end
 
-  def update
-    @consultation = Consultation.find(params[:id])
-    @consultation.patient = Patient.find(params[:patient_id])
-    @consultation.save
-    @consultation.update(consultation_params)
-    redirect_to new_consultation_email_path(params[:id])
-  end
+    def show
+      @consultation = Consultation.find(params[:id])
+    end
 
-  def add_tags
-    @consultation = Consultation.find(params[:id])
-    @patient = @consultation.patient
-    @consultation.tag_list
-    @i = 0
-    @consultation_templates = YAML.load_file("db/consultation_templates.yml")
-    params[:consultation][:tag_list].split(',').each do |t|
-      @consultation.tag_list.add(t)
+    def new
+      @consultation = Consultation.new
+    end
+
+    def create
+      @consultation = Consultation.new(consultation_params)
+      @consultation.save
+      redirect_to dashboard_path
+    end
+
+    def edit
+      @patient = Patient.find(params[:patient_id])
+      @consultation = @patient.consultations.last
+      @consultation_templates = YAML.load_file("db/consultation_templates.yml")
+      @consultation.patient = Patient.find(params[:patient_id])
+      @all_tagg_list = ActsAsTaggableOn::Tag.all
+      respond_to do |format|
+        format.html { redirect_to edit_patient_consultation_path(@patient) }
+        format.js
+      end
+    end
+
+    def update
+      @consultation = Consultation.find(params[:id])
+      @consultation.patient = Patient.find(params[:patient_id])
+      @consultation.save
+      @consultation.update(consultation_params)
+      redirect_to new_consultation_email_path(params[:id])
+    end
+
+    def add_tags
+      @consultation = Consultation.find(params[:id])
+      @patient = @consultation.patient
+      @consultation.tag_list
+      @i = 0
+      @consultation_templates = YAML.load_file("db/consultation_templates.yml")
+      params[:consultation][:tag_list].split(',').each do |t|
+        @consultation.tag_list.add(t)
+        @consultation.save
+      end
+      respond_to do |format|
+        format.html { redirect_to edit_patient_consultation_path(params[:id]) }
+        format.js
+      end
+    end
+
+    def remove_tags
+      @consultation = Consultation.find(params[:id])
+      @consultation.tag_list.remove(params[:tag_name])
       @consultation.save
     end
-    respond_to do |format|
-      format.html { redirect_to edit_patient_consultation_path(params[:id]) }
-      format.js
+
+    def destroy
+    end
+
+    private
+
+    def consultation_params
+      params.require(:consultation).permit(:patient_id, :status, :start_time, :end_time, :consultation_type, :required_documents, :patient_complain, :diagnostic, :care_plan, :prescription, :note_patient, :patient_note_attachment)
     end
   end
-
-  def remove_tags
-    @consultation = Consultation.find(params[:id])
-    @consultation.tag_list.remove(params[:tag_name])
-    @consultation.save
-  end
-
-  def destroy
-  end
-
-  private
-
-  def consultation_params
-    params.require(:consultation).permit(:patient_id, :status, :start_time, :end_time, :consultation_type, :required_documents, :patient_complain, :diagnostic, :care_plan, :prescription, :note_patient, :patient_note_attachment)
-  end
-end
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,22 +1,22 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/themes/material_blue.css" // Note this is important!
+    import flatpickr from "flatpickr"
+    import "flatpickr/dist/themes/material_blue.css" // Note this is important!
 
-flatpickr(".datepicker", {
-  altInput: true,
-  allowInput: true,
-  altFormat: "d F Y",
-})
+    flatpickr(".datepicker", {
+      altInput: true,
+      allowInput: true,
+      altFormat: "d F Y",
+    })
 
-flatpickr(".meetpicker", {
-  altInput: true,
-  allowInput: true,
-  altFormat: "d F Y",
-  time_24hr: true,
-})
+    flatpickr(".meetpicker", {
+      altInput: true,
+      allowInput: true,
+      altFormat: "d F Y",
+      time_24hr: true,
+    })
 
 
-flatpickr(".timepicker",{
-    enableTime: true,
-    noCalendar: true,
-    dateFormat: "H"
-})
+    flatpickr(".timepicker",{
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H"
+    })
