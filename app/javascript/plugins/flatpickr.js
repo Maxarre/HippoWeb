5813f3fc2b2089f3e64b9fb2b643631480324f6d
Tom Halimi <tomcompteperso@gmail.com>
2019-06-13 17:43:04 +0200

    flatpicker-cleaned exit /q \q

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,31 +1,22 @@
-    import flatpickr from "flatpickr"
-    import "flatpickr/dist/themes/material_blue.css" // Note this is important!
+import flatpickr from "flatpickr"
+import "flatpickr/dist/themes/material_blue.css" // Note this is important!
 
-    flatpickr(".datepicker", {
-      altInput: true,
-      allowInput: true,
-      altFormat: "d F Y",
-    })
+flatpickr(".datepicker", {
+  altInput: true,
+  allowInput: true,
+  altFormat: "d F Y",
+})
 
-<<<<<<< HEAD
-    flatpickr(".meetpicker", {
-      altInput: true,
-      allowInput: true,
-      altFormat: "d F Y",
-      time_24hr: true,
-    })
-
-
-    flatpickr(".timepicker",{
-        enableTime: true,
-        noCalendar: true,
-        dateFormat: "H"
-    })
-=======
 flatpickr(".meetpicker", {
   altInput: true,
   allowInput: true,
   altFormat: "d F Y",
   time_24hr: true,
 })
->>>>>>> 7665ad5ea34b1e3938764c806b82289232d2dd45
+
+
+flatpickr(".timepicker",{
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H"
+})
